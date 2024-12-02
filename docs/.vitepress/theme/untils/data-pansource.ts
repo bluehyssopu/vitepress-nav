6be79c312be856f4fd5324c3f31f4739e990d8d6
huzhipeng <1767807919@qq.com>
2024-12-02 15:11:43 +0800

    补充AIGC资源和四六级真题PDF

--- docs/.vitepress/theme/untils/data-pansource.ts
+++ docs/.vitepress/theme/untils/data-pansource.ts
@@ -19,9 +19,15 @@ export const NAV_DATA: NavData[] = [
       },
       {
         icon: 'https://nav.kzmqvq.com/logo.svg',
-        title: '英语真题四六级在线🔤',
+        title: '英语真题四六级在线网站🔤',
         desc: '在线的历年真题试题',
         link: 'https://zhenti.burningvocabulary.cn/'
+      },
+      {
+        icon: 'https://nav.kzmqvq.com/logo.svg',
+        title: '英语四级历年真题大全PDF（包含答案）🔤',
+        desc: '',
+        link: 'https://pan.quark.cn/s/bf12b618741a'
       },
       {
         icon: 'https://nav.kzmqvq.com/logo.svg',
@@ -83,7 +89,7 @@ export const NAV_DATA: NavData[] = [
         desc: '包括基础讲解、历年真题、华为配置专题等',
         link: 'https://pan.quark.cn/s/4a58aa56dc5f'
       }
-      
+
     ]
   },
   {
@@ -100,6 +106,233 @@ export const NAV_DATA: NavData[] = [
         title: '肖申克的救赎',
         desc: '希望之光穿透黑暗，自由之翼翱翔天际，人性之力战胜体制，不朽经典。',
         link: 'https://pan.quark.cn/s/5cdb90acc18c'
+      }
+    ]
+  },
+  {
+    title: 'AIGC资料',
+    items: [
+      {
+        icon: 'https://nav.kzmqvq.com/logo.svg',
+        title: '付费购买的AI课程大全+行业报告专区【134GB】',
+        desc: '',
+        link: 'https://pan.quark.cn/s/537ec8840a5d'
+      },
+      {
+        "icon": "",
+        "title": "【AI绘画软件】NovelAI最终版 webui版 无需额外下载安装！（汉化 可更新 已训练）",
+        "desc": "",
+        "link": "https://pan.quark.cn/s/c9cb8314eeb9"
+      },
+      {
+        "icon": "",
+        "title": "百度·文心一言AI·运营变现",
+        "desc": "",
+        "link": "https://pan.quark.cn/s/cd092f60fa93"
+      },
+      {
+        "icon": "",
+        "title": "万维钢·AI前沿（完结）",
+        "desc": "",
+        "link": "https://pan.quark.cn/s/bd0643caf686"
+      },
+      {
+        "icon": "",
+        "title": "12000 AI绘画关键词描述语",
+        "desc": "",
+        "link": "https://pan.quark.cn/s/9e045463184c"
+      },
+      {
+        "icon": "",
+        "title": "2023年6~7月最新AI行业报告合集（200份）",
+        "desc": "",
+        "link": "https://pan.quark.cn/s/12f8b0351646"
+      },
+      {
+        "icon": "",
+        "title": "2023年AIGC和ChatGPT的研究报告集合",
+        "desc": "",
+        "link": "https://pan.quark.cn/s/c173e00cd3f2"
+      },
+      {
+        "icon": "",
+        "title": "AI变现实战案例教程",
+        "desc": "",
+        "link": "https://pan.quark.cn/s/9be4ecbedcce"
+      },
+      {
+        "icon": "",
+        "title": "AI大师课直播——刺未4.9",
+        "desc": "",
+        "link": "https://pan.quark.cn/s/07d886bb9292"
+      },
+      {
+        "icon": "",
+        "title": "AI辅助设计训练营：基础入门与初步应用课程 深入实战课程",
+        "desc": "",
+        "link": "https://pan.quark.cn/s/eb81e1514beb"
+      },
+      {
+        "icon": "",
+        "title": "AI绘画大师班 Midjourney入门到精通",
+        "desc": "",
+        "link": "https://pan.quark.cn/s/18a34adb0a98"
+      },
+      {
+        "icon": "",
+        "title": "AI绘画速成课stable diffusion，?熟练运营stable diffusion，快速生成高品质设计图稿、B端设计3D图标",
+        "desc": "",
+        "link": "https://pan.quark.cn/s/62d1e4f3e15d"
+      },
+      {
+        "icon": "",
+        "title": "AI机器人报告-8月",
+        "desc": "",
+        "link": "https://pan.quark.cn/s/ad78aef6d923"
+      },
+      {
+        "icon": "",
+        "title": "AI人工智能2.0：每个人的人工智能课：从现在开始学习AI 拥抱未来 拥有AI",
+        "desc": "",
+        "link": "https://pan.quark.cn/s/dcc891f0e2b7"
+      },
+      {
+        "icon": "",
+        "title": "AI时代个人生存指南（内部管培资料流出)",
+        "desc": "",
+        "link": "https://pan.quark.cn/s/fd98ebe92046"
+      },
+      {
+        "icon": "",
+        "title": "AI商业智慧【AI 文案 PPT 图像 视频】",
+        "desc": "",
+        "link": "https://pan.quark.cn/s/be92a9b41414"
+      },
+      {
+        "icon": "",
+        "title": "AI行业最新资料大全（391份）",
+        "desc": "",
+        "link": "https://pan.quark.cn/s/7700a91d8059"
+      },
+      {
+        "icon": "",
+        "title": "AI造富训练营 让一部分人先用AI赚到第一个100万 让你快人一步抓住行业红利",
+        "desc": "",
+        "link": "https://pan.quark.cn/s/f9a7589c972b"
+      },
+      {
+        "icon": "",
+        "title": "ChatGPT第一课：武装我们第2大脑（虚拟人）",
+        "desc": "",
+        "link": "https://pan.quark.cn/s/9623df3f337d"
+      },
+      {
+        "icon": "",
+        "title": "ChatGPT大师班 从入门到精通",
+        "desc": "",
+        "link": "https://pan.quark.cn/s/2f2f2d494a48"
+      },
+      {
+        "icon": "",
+        "title": "ChatGPT运营秘诀与变现攻略（100节课）",
+        "desc": "",
+        "link": "https://pan.quark.cn/s/d6e4b84c976a"
+      },
+      {
+        "icon": "",
+        "title": "快刀青衣·给职场人的AI写作课（完结）",
+        "desc": "",
+        "link": "https://pan.quark.cn/s/eac45896163b"
+      },
+      {
+        "icon": "",
+        "title": "李一舟人工智能2.0",
+        "desc": "",
+        "link": "https://pan.quark.cn/s/8ff96adbb64b"
+      },
+      {
+        "icon": "",
+        "title": "刘飞· 如何利用AI进行商业设计（完结）",
+        "desc": "",
+        "link": "https://pan.quark.cn/s/2d96c8f809b1"
+      },
+      {
+        "icon": "",
+        "title": "Midjourney 绘画关键词 配图 12000",
+        "desc": "",
+        "link": "https://pan.quark.cn/s/0effd95176dd"
+      },
+      {
+        "icon": "",
+        "title": "李笑来·《谈AI时代的家庭教育》（完结）",
+        "desc": "",
+        "link": "https://pan.quark.cn/s/9fa5c67500a3"
+      },
+      {
+        "icon": "",
+        "title": "Midjourney入门到精通 AI绘图作画教学",
+        "desc": "",
+        "link": "https://pan.quark.cn/s/0d7b7a9b407c"
+      },
+      {
+        "icon": "",
+        "title": "Midjourney教程视频 - 场景实战篇",
+        "desc": "",
+        "link": "https://pan.quark.cn/s/632d9327e6f6"
+      },
+      {
+        "icon": "",
+        "title": "马馺·怎样用AI做PPT（完结）",
+        "desc": "",
+        "link": "https://pan.quark.cn/s/ef20db10becf"
+      },
+      {
+        "icon": "",
+        "title": "且曼AI智能绘画Midjourney",
+        "desc": "",
+        "link": "https://pan.quark.cn/s/8b017600a0a9"
+      },
+      {
+        "icon": "",
+        "title": "前沿课·吴军讲GPT（完结）",
+        "desc": "",
+        "link": "https://pan.quark.cn/s/565067f8f749"
+      },
+      {
+        "icon": "",
+        "title": "人人必修的AI启蒙课，启蒙课程，不含工具【鹤老师】",
+        "desc": "",
+        "link": "https://pan.quark.cn/s/644ef77967fa"
+      },
+      {
+        "icon": "",
+        "title": "StableDiffusion零基础入门课",
+        "desc": "",
+        "link": "https://pan.quark.cn/s/bb039147b353"
+      },
+      {
+        "icon": "",
+        "title": "玩赚ChatGPT",
+        "desc": "",
+        "link": "https://pan.quark.cn/s/5f2d37bb4f37"
+      },
+      {
+        "icon": "",
+        "title": "卓克·怎样用AI高效学习（完结）",
+        "desc": "",
+        "link": "https://pan.quark.cn/s/646b19d40275"
+      },
+      {
+        "icon": "",
+        "title": "野菩萨AI绘画资深课2023，全新课程，全新视野，知识重构，引领未来",
+        "desc": "",
+        "link": "https://pan.quark.cn/s/2941eab72a67"
+      },
+      {
+        "icon": "",
+        "title": "最新AIGC ChatGPT研究报告",
+        "desc": "",
+        "link": "https://pan.quark.cn/s/55ed8a83c526"
       }
     ]
   }
